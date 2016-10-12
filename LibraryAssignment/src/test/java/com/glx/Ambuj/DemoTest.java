1acae3a0990f912f8fde6c7128e0c5d9608dc5a5
amgupta <amgupta@GBWK-INA94101Y8.galaxy.lan>
2016-10-12 15:19:26 +0530

    Testing Push

---
+++ LibraryAssignment/src/test/java/com/glx/Ambuj/DemoTest.java
@@ -0,0 +1,11 @@
+package com.glx.Ambuj;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	@Test
+	public void jenkinstest(){
+		 System.out.println("Testing begins");
+	}
+}
